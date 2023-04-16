7e7691421a900f9d8c7375d9040ce8998b1c131f
AnaG.Sgit config --global user.mail anisnopek@gmail.com <anisnopek@gmail.com>
2023-04-16 20:08:03 -0300

    Cambios en el header y nav en html, css y js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
-const nav = document.querySelector(#"nav");
-const open = document.querySelector(#"open");
-const close = document.querySelector(#"close");
+const nav = document.querySelector("#nav");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
 
 open.addEventListener("click", () => {
-    nav.classList.add("visible");
+    nav.classList.add("nav-visible");
 })
 close.addEventListener("click", () => {
-    nav.classList.remove("visible");
+    nav.classList.remove("nav-visible");
 })

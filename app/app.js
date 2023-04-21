eeda52363a6a7740b6beadac92b97d3dbc5b4c10
AnaG.Sgit config --global user.mail anisnopek@gmail.com <anisnopek@gmail.com>
2023-04-21 20:43:59 -0300

    Incorporación de la Api con js, modificaciones en html y css

--- app/app.js
+++ app/app.js
@@ -8,3 +8,29 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     nav.classList.remove("nav-visible");
 })
+
+const URL_API = "https://rickandmortyapi.com/api/character/?page=5";
+
+fetch( URL_API )
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const characters = data.results;
+    const $containerGrid = document.getElementById("container-grid");
+
+    for (let i = 0; i < characters.length; i++) {
+      $containerGrid.innerHTML += `
+      <div class="grid-item">
+        <img src="${characters[i].image}">
+        <h3 class="characters-name">${characters[i].name}</h3>
+        <p>Gender:${characters[i].gender}</p>
+        <p>Species:${characters[i].species}</p>
+        <p>Status:${characters[i].status}</p>
+        <p>Origin:${characters[i].origin.name}</p>
+      </div>
+      `;
+    }
+  });
+
+
